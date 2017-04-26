5a150835105e3d13b4f7e8c3a9335b1e8b3be54a
Martin <skydiver@users.noreply.github.com>
2017-04-26 12:34:55 -0300

    * disable menubar * using config file

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 const electron = require('electron');
 const trayIcon = require('./tray');
-
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
 const path = require('path');
-const iconPath = path.join(__dirname, 'favicon-152.png');
+const config = require('./config.json');
 
 let mainWindow;
 
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const iconPath = path.join(__dirname, config.icon);
+
 function createWindow() {
 
+    let bounds = electron.screen.getPrimaryDisplay().bounds;
+    let x = bounds.x + ((bounds.width - config.width) / 2);
+    let y = bounds.y + ((bounds.height - config.height) / 2);
+
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: config.width,
+        height: config.height,
+        x: x,
+        y: y,
         icon: iconPath
     });
 
     mainWindow.loadURL("https://codeanywhere.com/editor/");
 
+    /* Start maximized */
+    if(config.maximized) {
+        mainWindow.maximize();
+    }
+
+    /* Hide menubar */
+    mainWindow.setMenu(null);
+
+    /* Show tray icon */
     trayIcon(electron, mainWindow, iconPath);
 
     // Open the DevTools.
@@ -28,11 +43,13 @@ function createWindow() {
         mainWindow = null
     });
 
+    /* Minimize to tray */
     mainWindow.on('minimize',function(event){
         event.preventDefault();
         mainWindow.hide();
     });
 
+    /* Close to tray */
     mainWindow.on('close', function (event) {
         if(!app.isQuiting){
             event.preventDefault();
